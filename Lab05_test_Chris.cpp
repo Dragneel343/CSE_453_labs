d53fe199fe36c7ddb926c677b15d0f35e6684e4a
Chris Bowen <50932159+mmxchris@users.noreply.github.com>
2021-10-14 22:58:27 -0400

    Update Lab05_test_Chris.cpp
    Added code to get current path

--- Lab05_test_Chris.cpp
+++ Lab05_test_Chris.cpp
@@ -1,18 +1,21 @@
 #include <string>
 #include <iostream>
+#include <filesystem>
+#include <locale>
 
 std::string getPath(std::string filename);
 
 int main()
  {
    std::string path1 = "cd test";
-   std::string path2 = "cd c:/test";
+   std::string path2 = "cd c:\\test";
+   std::cout << std::filesystem::current_path() << std::endl;
 
-   std::cout << "Enter a file path: ";
-   std::getline(std::cin, path1);
+   //std::cout << "Enter a file path: ";
+   //std::getline(std::cin, path1);
 
-   std::cout << "Enter a second file path: ";
-   std::getline(std::cin, path2);
+   //std::cout << "Enter a second file path: ";
+   //std::getline(std::cin, path2);
    
    std::cout << path1 << " --- " << getPath(path1) << std::endl;
    std::cout << path2 << " --- " << getPath(path2) << std::endl;
@@ -26,28 +29,36 @@ int main()
       std::cout << "Not A Homograph!" << std::endl;
    }
 
-   return 0;
+   return 0; 
 }
 
 std::string getPath(std::string filename)
 {
-   std::string tempPath;
-   if (filename[4] == ':')
-   {
-      //tempPath = "c:";
+   std::string Path; 
+
+   if (filename[4] != ':' )
+   {      
+      std::filesystem::path cwd = std::filesystem::current_path();
+      std::string currentPath = cwd.string();
+      
+      std::locale loc;
+      Path += std::tolower(currentPath[0], loc);
+      Path += currentPath[1];
+      Path += currentPath[2];      
+
       for (int i = 3; i < filename.length(); i++)
       {         
-         tempPath += filename[i];
-      }
-   }
-   else
-   {
-      tempPath = "c:/";
-      for (int i = 3; i < filename.length(); i++)
-      {
-         tempPath += filename[i];
+         Path += filename[i];
       }
    }
 
-   return tempPath;
+   else
+   {
+      for (int i = 3; i < filename.length(); i++)
+      {
+         Path += filename[i];
+      }
+   }
+
+   return Path;
 }
