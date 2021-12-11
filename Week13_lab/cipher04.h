54b039c9fd03246af41da280a29520484a07d403
Kylejbunn <92401902+Kylejbunn@users.noreply.github.com>
2021-12-10 21:27:02 -0500

    Update cipher04.h
    Kyle Bunn added cipher04.h as his submission for Lab13.

--- Week13_lab/cipher04.h
+++ Week13_lab/cipher04.h
@@ -12,10 +12,10 @@
 class Cipher04 : public Cipher
 {
 public:
-   virtual std::string getPseudoAuth()  { return "pseudocode author"; }
-   virtual std::string getCipherName()  { return "cipher name"; }
-   virtual std::string getEncryptAuth() { return "encrypt author"; }
-   virtual std::string getDecryptAuth() { return "decrypt author"; }
+   virtual std::string getPseudoAuth()  { return "Kyle Bunn"; }
+   virtual std::string getCipherName()  { return "Polybius Square Cipher"; }
+   virtual std::string getEncryptAuth() { return "Kyle Bunn"; }
+   virtual std::string getDecryptAuth() { return "Kyle Bunn"; }
 
    /***********************************************************
     * GET CIPHER CITATION
@@ -23,7 +23,11 @@ public:
     ***********************************************************/
    virtual std::string getCipherCitation()
    {
-      return std::string("citation");
+       std::string s;
+       s += "GeeksforGeeks.com (2021), ";
+       s += "\"Geeks for Geeks - Polybius Square Cipher\', \n   retrieved: 12/10/21 ";
+       s += "https://www.geeksforgeeks.org/polybius-square-cipher/";
+       return s;
    }
    
    /**********************************************************
@@ -34,12 +38,29 @@ public:
    {
       std::string str;
 
-      // TODO: please format your pseudocode
-      // The encrypt pseudocode
-      str =  "insert the encryption pseudocode\n";
+      // The 'default' Polybius Square
+      str = "\n *The Polybius Square*\n\n";
+      str += "   1 2 3 4 5\n";
+      str += " 1 A B C D E\n";
+      str += " 2 F G H I K\n";
+      str += " 3 L M N O P\n";
+      str += " 4 Q R S T U\n";
+      str += " 5 V W X Y Z\n\n";
 
-      // The decrypt pseudocode
-      str += "insert the decryption pseudocode\n";
+      // The encryptCase pseudocode
+      str += "encrypt(string)\n";
+      str += "   int row, collumn\n";
+      str += "   FOR each element of string assign a row and collumn converting each letter into a two digit pair \n";
+      str += "   IF the character is a 'k' row -1 and collumn +1 \n";
+      str += "   Else if the character is greater than j adjust for the combined 'i'/'j' square\n";
+      str += "   RETURN cipherText\n\n";
+
+      // The decryptCase pseudocode
+      str += "decrypt(cipherText, password)\n";
+      str += "   int row, collumn\n";
+      str += "   FOR each element of string look at two a numbered pair and convert into letters \n";
+      str += "   RETURN plainText\n\n";
+
 
       return str;
    }
@@ -51,9 +72,45 @@ public:
    virtual std::string encrypt(const std::string & plainText,
                                const std::string & password)
    {
-      std::string cipherText = plainText;
-      // TODO - Add your code here
-      return cipherText;
+      std::string s = plainText;
+      string cypherText;
+      int row;
+      int col;
+      
+
+      for (int i = 0; s[i]; i++) {
+
+          // finding row of the table
+          row = ceil((s[i] - 'a') / 5) + 1;
+
+          // finding column of the table
+          col = ((s[i] - 'a') % 5) + 1;
+
+          // if character is 'k'
+          if (s[i] == 'k') {
+              row = row - 1;
+              col = 5 - col + 1;
+          }
+
+          // if character is greater than 'j'
+          else if (s[i] >= 'j') {
+              if (col == 1) {
+                  col = 6;
+                  row = row - 1;
+              }
+              col = col - 1;
+          }
+           
+          cypherText.push_back(row);
+          cypherText.push_back(col);
+
+          
+      }
+      
+
+
+
+      return cypherText;
    }
 
    /**********************************************************
@@ -64,7 +121,30 @@ public:
                                const std::string & password)
    {
       std::string plainText = cipherText;
-      // TODO - Add your code here
+      
+      string returnText;
+
+      int row;
+      int col;
+      
+      char poly[5][5] = {
+          { 'a','b','c','d','e' },
+          { 'f','g','h','i','k' },
+          { 'l','m','n','o','p' },
+          { 'q','r','s','t','u' },
+          { 'v','w','x','y','z' }
+      };
+
+
+      for (int i = 0; plainText[i]; i++)
+      {
+          row = plainText[i];
+          col = plainText[i + 1];
+
+          returnText += poly[row][col];
+      }
+
+
       return plainText;
    }
 };
